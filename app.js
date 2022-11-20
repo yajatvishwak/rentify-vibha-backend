6395a2bfd53fef542bd8b53c71a2c2ad1350a3ff
yajatvishwak <yajat472001@gmail.com>
2022-11-20 14:21:32 +0530

    sighs

--- app.js
+++ app.js
@@ -77,30 +77,96 @@ app.post("/editprofile", (req, res) => {
   }
 });
 
-app.post("/add-listing" , (req, res) => {
-    const { title, desc, price, interval, uid , category, imgurl } = req.body;
-    const insert = db.prepare(
-        "INSERT INTO listings ( imgurl,title, desc, price, interval, uid , category) VALUES (@imgurl,@title, @desc, @price, @interval, @uid , @category)"
-      );
-  let info = insert.run({ imgurl,title, desc, price, interval, uid , category });
+app.post("/add-listing", (req, res) => {
+  const { title, desc, price, interval, uid, category, imgurl } = req.body;
+  const insert = db.prepare(
+    "INSERT INTO listings ( imgurl,title, desc, price, interval, uid , category) VALUES (@imgurl,@title, @desc, @price, @interval, @uid , @category)"
+  );
+  let info = insert.run({
+    imgurl,
+    title,
+    desc,
+    price,
+    interval,
+    uid,
+    category,
+  });
   if (info) {
-    res.send({ isCreated: true, code: "suc",  });
+    res.send({ isCreated: true, code: "suc" });
   } else {
     res.send({ isCreated: false, code: "err" });
   }
-})
-app.post("/get-listing" , (req, res) => {
-  
+});
+app.post("/get-listing", (req, res) => {
   const { lid } = req.body;
-    const select = db.prepare(
-        "SELECT * from listings where lid=@lid"
-      ).get({lid});
-      if (select) {
-        return res.send({ code: "suc", item: select });
-      }
-})
-
-app.listen(5000, () => { 
-  console.log("Server online at 5000");
+  const select = db
+    .prepare("SELECT * from listings where lid=@lid")
+    .get({ lid });
+  if (select) {
+    return res.send({ code: "suc", item: select });
+  }
+});
+app.post("/get-all-listings", (req, res) => {
+  const { lid } = req.body;
+  const select = db.prepare("SELECT * from listings").all({ lid });
+  if (select) {
+    return res.send({ code: "suc", item: select });
+  }
 });
 
+app.post("/add-item-to-rent", (req, res) => {
+  try {
+    const { lid, uid } = req.body;
+    // let user = db.prepare("SELECT * from users where uid=@uid").get({ uid });
+    // let listing = db
+    //   .prepare("SELECT * from listings where lid=@lid")
+    //   .get({ lid });
+    const insert = db.prepare(
+      "INSERT INTO user_renting (lid, uid) VALUES (@lid , @uid)"
+    );
+    insert.run({
+      uid,
+      lid,
+    });
+    res.send({ code: "suc", message: "item added" });
+  } catch (error) {
+    res.send({ code: "lol", message: "item could not be added" });
+  }
+});
+app.post("/get-profile", (req, res) => {
+  try {
+    const { uid } = req.body;
+    let listingsOfUser = db
+      .prepare("SELECT * from user_renting where uid=@uid")
+      .all({ uid });
+    let popListing = [];
+    for (const { lid } of listingsOfUser) {
+      console.log(lid);
+      let listing = db
+        .prepare("SELECT * from listings WHERE lid=@lid")
+        .get({ lid });
+      popListing.push(listing);
+    }
+
+    res.send({ code: "suc", userrenting: popListing });
+  } catch (error) {
+    console.log(error);
+    res.send({ code: "lol", message: "item could not be retrieved" });
+  }
+});
+app.post("/return-item", (req, res) => {
+  try {
+    const { uid, lid } = req.body;
+    let deleteListing = db
+      .prepare("DELETE from user_renting where lid=@lid and uid=@uid")
+      .run({ uid, lid });
+
+    res.send({ code: "suc" });
+  } catch (error) {
+    console.log(error);
+    res.send({ code: "lol", message: "item could not be deleted" });
+  }
+});
+app.listen(5000, () => {
+  console.log("Server online at 5000");
+});
